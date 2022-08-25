0ce9f0fad41cc2abb4561c22f9d9d3bbba8b9df0
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-08-25 23:08:04 +0300

    Updated Sidebar toggle

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,9 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() =>
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+                }
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
