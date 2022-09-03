6a64dd2f1a46ac36c5f5c2241aac65f4d547e199
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-09-03 21:26:02 +0300

    Updated 'Revenue Updates' in Ecommerce page

--- src/Pages/Ecommerce.jsx
+++ src/Pages/Ecommerce.jsx
@@ -35,28 +35,28 @@ const Ecommerce = () => {
             <div
               key={item.title}
               className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
-            ><button
-            type="button"
-            style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-            className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
-          >
-            {item.icon}
-          </button>
-          <p className="mt-3">
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
                 <span className="text-lg font-semibold">{item.amount}</span>
                 <span className={`text-sm text-${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
               <p className="text-sm text-gray-400  mt-1">{item.title}</p>
-          
-          </div>
+            </div>
           ))}
         </div>
       </div>
       <div flex gap-10 flex-wrap justify-center>
-      <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
-      <div className="flex justify-between">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+          <div className="flex justify-between">
             <p className="font-semibold text-xl">Revenue Updates</p>
             <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
@@ -65,9 +65,15 @@ const Ecommerce = () => {
                 </span>
                 <span>Expense</span>
               </p>
-      </div>
-      </div>
-      </div>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
