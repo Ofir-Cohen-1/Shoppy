2e35839e3fe30ef7eca226298d631931b589e10f
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-09-04 23:36:29 +0300

    Updated 'Revenue Updates' in Ecommerce page

--- src/Pages/Ecommerce.jsx
+++ src/Pages/Ecommerce.jsx
@@ -84,6 +84,11 @@ const Ecommerce = () => {
                 </p>
                 <p className="text-gray-500 mt-1">Budget</p>
               </div>
+              <div className="mt-8">
+                <p className="text-3xl font-semibold">$48,487</p>
+
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
             </div>
           </div>
         </div>
